fc3df101d49dd07b1920092d0a71cef6230e2f34
Sae Pasomba <saepasomba12@gmail.com>
2022-05-25 00:45:25 +0700

    Add get cart

--- checkout.py
+++ checkout.py
@@ -2,7 +2,40 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+cart = {
+    "johndoe" : [
+        {
+            "product" : {
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+            },
+            "amount": 2
+        },
+
+    ]
+
+}
+
+@app.get("/checkout/cart")
+async def root(username: str):
+    if username not in cart {
+        cart[username] = []
+    }
+    
+    grandTotal = 0
+    items = []
+
+    for item in cart[username]:
+        totalPrice = 0
+        items["product"] = item
+        grandTotal += item["product"]["price"] * item["amount"]
+        totalPrice += item["product"]["price"] * item["amount"]
+        
+        items.append({
+            "product": item,
+            "amount": item["amount"],
+            "totalPrice": totalPrice
+        })
+
+
+    return {"username": username, "grandTotal": grandTotal, "items": items}
+
